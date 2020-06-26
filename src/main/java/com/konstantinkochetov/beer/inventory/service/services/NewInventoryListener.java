02a7a435b53916f346c5eb76d2b111874cc98d81
konstantin kochetov <kochetov0824@gmail.com>
2020-06-26 08:50:54 +0200

    beer + invetory jms

---
+++ src/main/java/com/konstantinkochetov/beer/inventory/service/services/NewInventoryListener.java
@@ -0,0 +1,31 @@
+package com.konstantinkochetov.beer.inventory.service.services;
+
+import com.konstantinkochetov.common.events.NewInventoryEvent;
+import com.konstantinkochetov.beer.inventory.service.config.JmsConfig;
+import com.konstantinkochetov.beer.inventory.service.domain.BeerInventory;
+import com.konstantinkochetov.beer.inventory.service.repositories.BeerInventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event){
+
+        log.debug("Got Inventory: " + event.toString());
+
+        beerInventoryRepository.save(BeerInventory.builder()
+                .beerId(event.getBeerDto().getId())
+                .upc(event.getBeerDto().getUpc())
+                .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                .build());
+    }
+
+}
